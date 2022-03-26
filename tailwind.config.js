433bec0ca966d544f844cf4f667bb7b45fe0db61
Muhammad Ridwan <muh.ridwan.777@gmail.com>
2022-03-26 23:50:52 +0700

    add local tailwind and update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./ridwan.html"],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        coklat: '#FDF6F4',
+        coklatua: '#795138',
+      },
+      fontFamily: {
+         lora: "'Lora', serif",
+         clicker: "'Clicker Script', cursive",
+      }
+    }
+  },
+  plugins: [],
+}
